7f26625f0c138a0fb083e435656d18ddcc7ba3fd
Markmbithi <markmbila@gmail.com>
2018-03-28 12:37:59 +0300

    Use of different React Components
    Showcases the use of different react components on a single component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from './Clock';
+import {UserForm, Clock, LoginControl, DisplayArray, DisplayObjectArray} from './MyControls';
+import Toggle from './Toggle';
 
 const myname="Mark"
 class App extends Component {
@@ -10,14 +11,33 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to My React Showcase</h1>
         </header>
         <p className="App-intro">
-          I am {myname} and I got started, edit <code>src/App.js</code> and save to reload.
+          I am {myname} and I got started. This app comprises different components and technologies just to showcase what can be done with react
         </p>
         <p>
-        <Clock/>
+        <b>Time Now:</b> <Clock/>
         </p>
+        <div>
+        <Toggle/>
+        </div>
+        <div>
+        <h2> Toggle between login and logout button and the display message</h2>
+        <LoginControl/>
+        </div>
+        <div>
+        <h2>Normal array displayed using unordered list</h2>
+        <DisplayArray/>
+        </div>
+        <div>
+        <h2>Display an array of objects contents</h2>
+        <DisplayObjectArray/>
+        </div>
+        <div>
+        <h2>Usage of Forms</h2>
+        <UserForm/>
+        </div>
       </div>
     );
   }
